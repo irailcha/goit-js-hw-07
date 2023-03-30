5ae3622d910964b16244f0cc0db7d22a0186d62a
Iryna Karpusenko <karpusenko.irina.13@gmail.com>
2023-03-30 17:21:03 +0300

    Домашне завдання7
    Виконано частину першої частини дз. виконано друге завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+const galleryContainer=document.querySelector('.gallery');
+
+
+function createGalleryItemsList(galleryItems) {
+    return galleryItems.map(item => {
+      const { preview, original, description } = item;
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              data-alt="${description}"
+            />
+          </a>
+        </li>
+      `;
+    }).join('');
+  }
+
+  const galleryMarkUp=createGalleryItemsList(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkUp);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+
+function onGalleryContainerClick(event){
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__item')) {
+    return;
+  }
+
+}
+
+ 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+const galleryContainer=document.querySelector('.gallery');
+
+
+function createGalleryItemsList(galleryItems) {
+    return galleryItems.map(item => {
+      const { preview, original, description } = item;
+      return `
+      <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+   </li>
+      `;
+    }).join('');
+  }
+
+  const galleryMarkUp=createGalleryItemsList(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkUp);
+
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+  });
+
